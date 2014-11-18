d0539b679b69931ba1bcaaaf2b595c6b2ea46e26
Yang <ipv6.elata@gmail.com>
2014-11-18 23:50:49 +0800

    proxy

---
+++ proxy.js
@@ -0,0 +1,14 @@
+var http = require('http'),
+    httpProxy = require('http-proxy');
+
+
+var proxy = httpProxy.createProxyServer({});
+
+
+var server = http.createServer(function(req, res) {
+  console.log(req.url);
+  proxy.web(req, res, { target: 'http://www.speedtest.net',timeout:1000*1000 });
+});
+
+console.log("listening on port 3000")
+server.listen(3000);
